6b532c2a6b042d2dabe96689e2ed129ce46f1601
Eric Li <38477812+EricLi-Dev@users.noreply.github.com>
2024-03-05 10:41:50 -0500

    Dockerizes MongoDB + Mongo-Express + Backend (#15)
    * Init Setup Code + Setup Instructions in README (#3)
    
    * initial commit
    
    * Project information
    
    * added setup instructions to README
    
    ---------
    
    Co-authored-by: Jubayer <jubayer7a@gmail.com>
    Co-authored-by: EricLi-Dev <johnlee888123456@gmail.com>
    
    * Added mongodb and mongoose, initialized db (#9)
    
    * Added mongodb and mongoose, initialized db
    
    * adds MongoDB container to docker-compose | adds DB var name to console print
    
    * fixes README new line problems
    
    * adds Docker installation link
    
    ---------
    
    Co-authored-by: Henry Fan <henryfan@henrys-air-2.mynetworksettings.com>
    Co-authored-by: EricLi-Dev <johnlee888123456@gmail.com>
    
    * Dockerized mongo + mongo-express + backend
    
    * adds updated docs for dockerization
    
    ---------
    
    Co-authored-by: Dulina Gunasekara <83606238+Dulinag@users.noreply.github.com>
    Co-authored-by: Jubayer <jubayer7a@gmail.com>
    Co-authored-by: Henry Fan <68411149+henryfan03@users.noreply.github.com>
    Co-authored-by: Henry Fan <henryfan@henrys-air-2.mynetworksettings.com>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-47-245.us-east-2.compute.internal>

--- server/server.js
+++ server/server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const cors = require('cors')
 const app = express();
 
+const mongoose = require('mongoose');
+const DB_name  = "FaceStylizerDB";
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/${DB_name}');
+    console.log(`Successfully connected to ${DB_name}`);
+}
+
 app.use(cors());
 
 app.get("/api/home", (req, res) => {
@@ -12,3 +22,4 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
