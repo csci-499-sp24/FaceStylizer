85c434b1e2e30ccc52db4123790f1e1b2aec3f1d
Jubayer <jubayer7a@gmail.com>
2024-03-05 22:44:56 -0500

    minor front page edits

--- client/components/upload.jsx
+++ client/components/upload.jsx
@@ -57,7 +57,7 @@ function FileUpload() {
           <section className="h-full overflow-auto p-8 w-full h-full flex flex-col">
             <header className="border-dashed border-2 border-gray-400 py-12 flex flex-col justify-center items-center">
               <p className="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
-                <span>Drag and drop your Pictures</span>&nbsp;<span>files anywhere or</span>
+                <span>Drag and Drop Files</span>
               </p>
               <input id="hidden-input" type="file" multiple className="hidden" />
               <button id="button" className="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none">
@@ -100,7 +100,7 @@ function FileUpload() {
 
           <footer className="flex justify-end px-8 pb-8 pt-4">
             <button id="submit" className="rounded-sm px-3 py-1 bg-yellow-500 hover:bg-yellow-400 text-white focus:shadow-outline focus:outline-none">
-              Upload now
+              Upload
             </button>
             <button onClick={handleCancel} className="ml-3 rounded-sm px-3 py-1 hover:bg-gray-300 focus:shadow-outline focus:outline-none">
               Cancel
