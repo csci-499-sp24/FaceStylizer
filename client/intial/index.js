8a4aa6c8de670f5636e34b3dcda151ea0832e5c9
unknown <dulinagunasekara@gmail.com>
2024-03-05 21:01:24 -0500

    added arrow, image, upload, Navbar Component

---
+++ client/intial/index.js
@@ -0,0 +1,41 @@
+// import React, {useEffect, useState} from 'react'
+
+// function Index() {
+  
+//   const [message, setMessage] = useState("Loading")
+
+//   console.log(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home")
+//   useEffect(() => {
+//     fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home").then(
+//       response => response.json()
+//     ).then(
+//       data => {
+//         console.log(data)
+//         setMessage(data.message)
+//       }
+//     )
+//   }, [])
+
+//   return (
+//     <div>
+//       <div>Return message from server</div>
+//       <div>{message}</div>
+//     </div>
+//   )
+// }
+
+// export default Index
+
+// 
+
+import React from 'react';
+
+function Index() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Index;
