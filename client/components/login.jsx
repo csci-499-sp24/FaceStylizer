0677a6fb73b24b546ed4d48c2271a7a7956a94a5
jubuyer <jubayer7a@gmail.com>
2024-03-16 01:18:27 -0400

    need to resolve reactstrap issues

---
+++ client/components/login.jsx
@@ -0,0 +1,49 @@
+// import React from "react";
+
+// // reactstrap components
+// import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+
+// function Login() {
+//   const [modalOpen, setModalOpen] = React.useState(false);
+//   return (
+//     <>
+//       <Button
+//         color="primary"
+//         type="button"
+//         onClick={() => setModalOpen(!modalOpen)}
+//       >
+//         Launch demo modal
+//       </Button>
+//       <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
+//         <div className=" modal-header">
+//           <h5 className=" modal-title" id="exampleModalLabel">
+//             Modal title
+//           </h5>
+//           <button
+//             aria-label="Close"
+//             className=" close"
+//             type="button"
+//             onClick={() => setModalOpen(!modalOpen)}
+//           >
+//             <span aria-hidden={true}>×</span>
+//           </button>
+//         </div>
+//         <ModalBody>...</ModalBody>
+//         <ModalFooter>
+//           <Button
+//             color="secondary"
+//             type="button"
+//             onClick={() => setModalOpen(!modalOpen)}
+//           >
+//             Close
+//           </Button>
+//           <Button color="primary" type="button">
+//             Save changes
+//           </Button>
+//         </ModalFooter>
+//       </Modal>
+//     </>
+//   );
+// }
+
+// export default Login;
