cc31f386a05258af4e3d5101acd9284721c19470
jubuyer <jubayer7a@gmail.com>
2024-03-16 02:21:22 -0400

    component code to be updated using demo

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,49 +1,50 @@
-// import React from "react";
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// // reactstrap components
-// import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+// reactstrap components
+import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
 
-// function Login() {
-//   const [modalOpen, setModalOpen] = React.useState(false);
-//   return (
-//     <>
-//       <Button
-//         color="primary"
-//         type="button"
-//         onClick={() => setModalOpen(!modalOpen)}
-//       >
-//         Launch demo modal
-//       </Button>
-//       <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
-//         <div className=" modal-header">
-//           <h5 className=" modal-title" id="exampleModalLabel">
-//             Modal title
-//           </h5>
-//           <button
-//             aria-label="Close"
-//             className=" close"
-//             type="button"
-//             onClick={() => setModalOpen(!modalOpen)}
-//           >
-//             <span aria-hidden={true}>×</span>
-//           </button>
-//         </div>
-//         <ModalBody>...</ModalBody>
-//         <ModalFooter>
-//           <Button
-//             color="secondary"
-//             type="button"
-//             onClick={() => setModalOpen(!modalOpen)}
-//           >
-//             Close
-//           </Button>
-//           <Button color="primary" type="button">
-//             Save changes
-//           </Button>
-//         </ModalFooter>
-//       </Modal>
-//     </>
-//   );
-// }
+function Login() {
+  const [modalOpen, setModalOpen] = React.useState(false);
+  return (
+    <>
+      <Button
+        color="primary"
+        type="button"
+        onClick={() => setModalOpen(!modalOpen)}
+      >
+        Launch demo modal
+      </Button>
+      <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
+        <div className=" modal-header">
+          <h5 className=" modal-title" id="exampleModalLabel">
+            Modal title
+          </h5>
+          <button
+            aria-label="Close"
+            className=" close"
+            type="button"
+            onClick={() => setModalOpen(!modalOpen)}
+          >
+            <span aria-hidden={true}>×</span>
+          </button>
+        </div>
+        <ModalBody>...</ModalBody>
+        <ModalFooter>
+          <Button
+            color="secondary"
+            type="button"
+            onClick={() => setModalOpen(!modalOpen)}
+          >
+            Close
+          </Button>
+          <Button color="primary" type="button">
+            Save changes
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
+}
 
-// export default Login;
+export default Login;
