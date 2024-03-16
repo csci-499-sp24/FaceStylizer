c8273750db15ba7f1ba36234c03ab324c0538458
jubuyer <jubayer7a@gmail.com>
2024-03-16 13:50:31 -0400

    added form to modal

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,36 +1,70 @@
 import React from "react";
-import Image from 'next/image';
+import 'tailwindcss/tailwind.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // reactstrap components
-import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+import { Button, 
+  Modal, 
+  ModalBody, 
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input
+ } from "reactstrap";
 
 function Login() {
   const [modalOpen, setModalOpen] = React.useState(false);
   return (
     <>
       <Button
-        color="primary"
-        type="button"
         onClick={() => setModalOpen(!modalOpen)}
       >
         Sign In
       </Button>
       <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
-        <div className=" modal-header">
-          <h5 className=" modal-title" id="exampleModalLabel">
-            Modal title
+        <div className="text-center m-3">
+          <h5 className="text-gray-600 text-sm font-bold">
+            Sign in with
           </h5>
-          <button
-            aria-label="Close"
-            className=" close"
-            type="button"
-            onClick={() => setModalOpen(!modalOpen)}
-          >
-            <span aria-hidden={true}>×</span>
-          </button>
         </div>
-        <ModalBody>...</ModalBody>
+        <div className=" btn-wrapper text-center">
+          <button
+              className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+              color="default"
+              href="#"
+              onClick={(e) => e.preventDefault()}
+            >
+              <img alt="..." class="w-5 mr-1" src="https://demos.creative-tim.com/notus-js/assets/img/google.svg"/>
+              Google 
+            </button>
+        </div>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="exampleEmail">
+                Email
+              </Label>
+              <Input
+                id="exampleEmail"
+                name="email"
+                placeholder="Email"
+                type="email"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">
+                Password
+              </Label>
+              <Input
+                id="examplePassword"
+                name="password"
+                placeholder="Password"
+                type="password"
+              />
+            </FormGroup>
+          </Form>
+        </ModalBody>
         <ModalFooter>
           <Button
             color="secondary"
