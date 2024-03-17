6e8449db29d57e63450ea76b12460e95bd4e82f9
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2024-03-17 14:40:14 -0400

    added a gallery page

---
+++ client/pages/app/gallery/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GalleryPage() {
+  return (
+    <div>
+      <h1>You are  successfully to GalleryPage</h1>
+    </div>
+  );
+}
+
+export default GalleryPage;
