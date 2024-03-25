b5394c19a81cb0449f30d129196029d57d1fc2e7
unknown <dulinagunasekara@gmail.com>
2024-03-24 21:14:13 -0400

    added instruction

--- client/components/MlUi.jsx
+++ client/components/MlUi.jsx
@@ -47,8 +47,13 @@ function MlUi({ images, onBack }) {
 
     return (
         <div className="p-8 flex flex-col items-center min-h-screen bg-white">
+
             <div className="flex justify-center items-center mb-8 overflow-x-auto">
-                {displayedImages.map((image, index) => (
+            <h1 className="text-2xl font-bold text-yellow-500 animate-bounce">Click Image to FaceStylize</h1>
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+    </svg>
+                    {displayedImages.map((image, index) => (
                     <img
                         key={index}
                         src={image}
