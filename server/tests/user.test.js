abc5269434570bf4e1df1327e8e9d2a68331e840
Henry Fan <henryfan@henrys-air-2.mynetworksettings.com>
2024-04-01 01:48:34 -0400

    Added test suite for Jest

---
+++ server/tests/user.test.js
@@ -0,0 +1,30 @@
+const {MongoClient} = require('mongodb');
+const db_name       = process.env.DB_NAME || "FaceStylizerDB";
+const mongoUrl      = process.env.MONGO_URL || "mongodb://root:password@localhost:27017";
+
+describe('insert', () => {
+    let connection;
+    let db;
+
+    beforeAll(async () => {
+        connection = await MongoClient.connect(mongoUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        db = await connection.db(db_name);
+    });
+
+    afterAll(async () => {
+        await connection.close();
+    });
+
+    it('should insert a doc into collection', async () => {
+        const users = db.collection('users');
+
+        const mockUser = {_id: 'some-user-id', name: 'John'};
+        await users.insertOne(mockUser);
+
+        const insertedUser = await users.findOne({_id: 'some-user-id'});
+        expect(insertedUser).toEqual(mockUser);
+    });
+});
