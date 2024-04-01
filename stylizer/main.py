93c088aee838dd4b823515a0bf9e6f13a8b1f18f
EricLi-Dev <johnlee888123456@gmail.com>
2024-03-31 20:13:36 -0400

    stylization logic

--- stylizer/main.py
+++ stylizer/main.py
@@ -4,7 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from stylizer import generatePretrainedStyle
 import shutil
 import glob, os
-from mangum import Mangum
 import uvicorn
 
 app = FastAPI()
@@ -16,27 +15,30 @@ async def root():
 
 @app.get("/stylizer/getPretrainedStyle", response_class=FileResponse)
 async def getPretrainedStyle(inputFile: UploadFile = File(...)):
-    # # Purge output files directory
-    # if os.path.isdir(OUTPUT_DIR):
-    #     shutil.rmtree(OUTPUT_DIR)
-    # 
-    # os.mkdir(OUTPUT_DIR)
-    #
-    # # Writes file to system
-    # path = f"JoJoGAN/test_input/{inputFile.filename}"
-    # with open(path, 'w+b') as file:
-    #     shutil.copyfileobj(inputFile.file, file)
-    # 
-    # # Generate pretrained style on file
-    # generatePretrainedStyle(inputFile.filename)
-    # 
-    # # Purge input images
-    # name = inputFile.filename.split(".")[0]
-    # for f in glob.glob(f"JoJoGAN/test_input/{name}*"):
-    #     os.remove(f)
+    # Purge output files directory
+    print("Purging output files directory")
+    if os.path.isdir(OUTPUT_DIR):
+        shutil.rmtree(OUTPUT_DIR)
+     
+    os.mkdir(OUTPUT_DIR)
 
-    # return f"{OUTPUT_DIR}/{inputFile.filename}_4_results.jpg"
-    return f"{OUTPUT_DIR}/download.jpeg_4_results.jpg"
+    # Writes file to system
+    print("Writing input file to JoJoGAN/test_input")
+    path = f"JoJoGAN/test_input/{inputFile.filename}"
+    with open(path, 'w+b') as file:
+        shutil.copyfileobj(inputFile.file, file)
+     
+    # Generate pretrained style on file
+    print("Generating pretrained style on input file")
+    res = generatePretrainedStyle(inputFile.filename)
+     
+    # Purge input images
+    print("Purge input images from JoJoGAN/test_input")
+    name = inputFile.filename.split(".")[0]
+    for f in glob.glob(f"JoJoGAN/test_input/{name}*"):
+        os.remove(f)
+
+    return f"{OUTPUT_DIR}/{inputFile.filename}_4_results.jpg"
 
 
 if __name__ == "__main__":
