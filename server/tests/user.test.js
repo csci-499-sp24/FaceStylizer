7581da0b6cec171dbf8bb781a92da54ce24ffb6c
Henry Fan <henryfan@henrys-air-2.mynetworksettings.com>
2024-04-01 16:03:29 -0400

    Changed test case

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -21,7 +21,7 @@ describe('insert', () => {
     it('should insert a doc into collection', async () => {
         const users = db.collection('users');
 
-        const mockUser = {_id: 'some-user-id', name: 'John'};
+        const mockUser = {_id: 'some-user-id', username: 'johndoe', password: 'password'};
         await users.insertOne(mockUser);
 
         const insertedUser = await users.findOne({_id: 'some-user-id'});
