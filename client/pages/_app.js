f7b16bd22eb5fe7e99699f31189fc371aac08408
Eric Li <38477812+EricLi-Dev@users.noreply.github.com>
2024-04-15 22:28:28 -0400

    Revert "Integrating Google Authentication to Sign Up Form"

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,6 @@
 import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { AuthProvider } from '../context/authContext';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />;
-    </AuthProvider>
-  ) 
+  return <Component {...pageProps} />;
 }
