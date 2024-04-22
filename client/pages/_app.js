f54f3e663a690b85b1c914026dabbd96e640e365
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2024-04-22 19:13:32 -0400

    resolved docker build issues due to google wrapping

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { AuthProvider } from '../context/authContext';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AuthProvider } from "../context/authContext";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />;
+      <GoogleOAuthProvider clientId="593381902699-jdfocdq7nnpnsbjehq5sedfs6cf8k7b8.apps.googleusercontent.com">
+        <Component {...pageProps} />;
+      </GoogleOAuthProvider>
     </AuthProvider>
-  ) 
+  );
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -3,15 +3,14 @@ import CenteredImage from "@/components/mainimage";
 import FileUpload from "@/components/upload";
 import ArrowDown from "@/components/arrow";
 import React from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function Index() {
   return (
-    <GoogleOAuthProvider clientId="593381902699-jdfocdq7nnpnsbjehq5sedfs6cf8k7b8.apps.googleusercontent.com">
-         <RHeader/>
+    <>
+        <RHeader/>
         <CenteredImage/>
         <FileUpload/>
-    </GoogleOAuthProvider>
+    </>
     
   );
 }
