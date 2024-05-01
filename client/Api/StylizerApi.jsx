436c933c5c2501583c5a5a42a4f1c5a5de659385
EricLi-Dev <johnlee888123456@gmail.com>
2024-04-30 23:41:41 -0400

    changes the stylizer base API endpoint

--- client/Api/StylizerApi.jsx
+++ client/Api/StylizerApi.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const STYLIZER_CLOUD_RUN_ENDPOINT_URL = 
     process.env.NODE_ENV === "production"
-        ? `${process.env.NEXT_PUBLIC_STYLIZER_URL}`
-        : "http://localhost:8080/";
+        ? `http://${process.env.NEXT_PUBLIC_IP}:8090/`
+        : "http://localhost:8090/";
 
 const StylizerApi = axios.create({
     baseURL: STYLIZER_CLOUD_RUN_ENDPOINT_URL,
