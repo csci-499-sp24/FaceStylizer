0bbd7c21c0278edea8bab7a70bafc1b7cde6bcd7
EricLi-Dev <johnlee888123456@gmail.com>
2024-04-30 23:42:19 -0400

    changes the port of stylizer webserver

--- stylizer/main.py
+++ stylizer/main.py
@@ -47,4 +47,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8090)))
