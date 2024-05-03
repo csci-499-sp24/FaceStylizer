4e51c93b38b471b835a42d167993f2796d10e607
unknown <dulinagunasekara@gmail.com>
2024-05-03 19:28:43 -0400

    donerr

--- client/components/upload.jsx
+++ client/components/upload.jsx
@@ -4,6 +4,7 @@ import StylizerApi from '@/Api/StylizerApi';
 import { useDropzone } from "react-dropzone";
 import ConfirmationModal from './ResponsePop';
 import CustomImageUpload from './custom';
+import ImageDropzone from './imagedrop';
 
 
 function FileUpload() {
@@ -20,6 +21,7 @@ function FileUpload() {
     const [modalOpen, setModalOpen] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
     const [isCustomSelected, setIsCustomSelected] = useState(false);
+
     const [custom, setCustom] = useState(null);
     const fileInputRef = useRef(null);
 
@@ -120,63 +122,43 @@ function FileUpload() {
         setSelectedStyle(null);
         setCustom(null);
     };
-
+    const handleImageRemoveCustom = (index) => {
+      // const updatedImages = [...displayedImages];
+      // updatedImages.splice(index, 1);
+      // setDisplayedImages(updatedImages);
+      // setSelectedImage(null);
+      // setSelectedStyle(null);
+      setCustom(null);
+  };
     return (
       <div className="flex flex-col items-center justify-center p-6 min-h-screen pt-40">
-   <div
-        {...getRootProps({ className: "dropzone" })}
-        className="w-full max-w-4xl mr-4"      >
-        <input {...getInputProps()} />
-        <div 
-          className={` rounded-lg p-4 ${
-            selectedImage ? "" : "border-gray-300"
-          }`}
-        >
-          {!selectedImage ? (
-                 <div className="text-gray-500 text-center">
-                 <img
-                   src="https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png" 
-                   alt="Placeholder"
-                   className="mx-auto max-w-full max-h-96 rounded"
-                 />
-                                  <p>Drag and drop some files here, or click to select files</p>
-
-               </div>
-          ) : (
-            <div className="relative">
-              <img
-                src={selectedImage}
-                alt="Preview"
-                className="rounded mx-auto max-w-full max-h-96"
-              />
-
-
-              <button
-                onClick={handleSwitchImage}
-                className="absolute top-0 right-0 m-2 bg-white rounded-full p-1 shadow-lg"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-      <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-      </svg>
-
-              </button>
-
-              
-              <button
-        onClick={handleClearImage}
-      className="absolute top-0 right-10 m-2 bg-white rounded-full p-1 shadow-lg"
->
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-     <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-      </svg>
-
-        </button>
-
-            </div>
-          )}
+<div className="flex">
+    <div className="flex-1 w-100">
+        <ImageDropzone
+            getRootProps={getRootProps}
+            getInputProps={getInputProps}
+            selectedImage={selectedImage}
+            handleSwitchImage={handleSwitchImage}
+            handleClearImage={handleClearImage}
+        />
+    </div>
+    {isCustomSelected && (
+        <div className="flex-1 w-64">
+            <CustomImageUpload
+                handleFileSelect={handleFileSelect}
+                handleImageRemove={handleImageRemoveCustom}
+                handleSwitchImage={handleSwitchImage}
+                Custom={custom}
+            />
         </div>
+    )}
+</div>
+
+    
         <div className="w-full max-w-4xl mt-4">
         <div className="w-full flex justify-center items-center">
+
+          
 
         <select
                     id="restaurantImage"
@@ -192,15 +174,7 @@ function FileUpload() {
                     <option value="custom">Custom</option>
                 </select>
                 </div>
-                {isCustomSelected && (
-      <CustomImageUpload
-        handleFileSelect={handleFileSelect}
-        handleImageRemove={handleImageRemove}
-        handleSwitchImage={handleSwitchImage}
-        Custom={custom}
-      />
-    )}
-                </div>
+
            </div>
             <div className="w-full md:w-3/4 mt-4 flex justify-center items-center">
                 {/* <button
