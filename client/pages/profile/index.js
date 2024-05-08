fa855b362f27170188e095868800c2f0f04ce376
jubuyer <jubayer7a@gmail.com>
2024-05-08 14:36:45 -0400

    UI change

--- client/pages/profile/index.js
+++ client/pages/profile/index.js
@@ -95,7 +95,19 @@ const ProfileCard = () => {
         <RHeader/>
 
       <div className="p-8 bg-white shadow mt-24">
-        <div className="grid grid-cols-1 md:grid-cols-3">
+        
+        <div className="mt-40 text-center border-b pb-12">
+         
+          <div className="px-25 display-flex place-content-center w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl -mt-24 mb-4 flex items-center justify-center text-indigo-500">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24" viewBox="0 0 20 20" fill="#B8860B">
+              <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+            </svg>
+          </div>
+          <h1 className="text-4xl font-medium text-gray-700 mb-5">
+                {username}
+          </h1>
+
+          <div className="grid grid-cols-1 md:grid-cols-3">
           <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0"></div>
           <div className="place-content-center"></div>
           <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
@@ -108,16 +120,6 @@ const ProfileCard = () => {
                         </div>
           </div>
         </div>
-        <div className="mt-40 text-center border-b pb-12">
-         
-          <div className="px-25 display-flex place-content-center w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl -mt-24 mb-4 flex items-center justify-center text-indigo-500">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24" viewBox="0 0 20 20" fill="#B8860B">
-              <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
-            </svg>
-          </div>
-          <h1 className="text-4xl font-medium text-gray-700">
-                {username}
-          </h1>
           <p className="mt-8 text-gray-500"><b>Enjoy Stylizing your best Selfies</b></p>
           <p className="mt-2 text-gray-500"><b>FaceStylizer is a Non-Profit Organization</b></p>
         </div>
